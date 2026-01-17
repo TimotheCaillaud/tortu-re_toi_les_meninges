7cbbd39d967c97227bc7389cab06c87002d72cfd
Timothé <timothe.caillaud@gmail.com>
2026-01-17 19:01:58 +0100

    fix compile failed

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface ButtonProps {
   children: React.ReactNode;
   variant?: "primary" | "secondary";
-  onClick?: () => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   type?: "button" | "submit" | "reset";
   className?: string;
   disabled?: boolean;
